b68632f60f83eeff0c4f494aceb6fb618bb8bbe8
mikami3heart <mikami3heart@gmail.com>
2016-11-10 17:00:35 +0900

    check count bug

---
+++ result/readme.txt
@@ -0,0 +1,4 @@
+Set the address in the Web browser:
+	http://togetsu.aics10.riken.jp:18080/flops/tasks/taskID/result/
+Then, push the GET" button.
+	Then, the tar ball including the analyzed JSON files is downloaded.
---
+++ src/check_counts.f90
@@ -0,0 +1,74 @@
+
+subroutine sub_fcount(a,b,c,n)
+integer :: n
+real*8 a(n), b(n), c(n)
+
+a123=a(1)+a(2)+a(3)
+b123=b(1)+b(2)+b(3)
+
+do i=1,n
+c(i)=0.1/(a(i)+b(i))
+end do
+do i=1,n
+c(i)=c(i)+a(i)*b(i)
+end do
+
+c(n)=c(n)+a123+b123
+return
+end
+
+
+subroutine sub_ext(a,b,c,n)
+integer :: n
+real*8 a(n), b(n), c(n)
+do i=1,n
+c(i)=log(exp( a(i)*b(i) ))
+end do
+return
+end
+
+
+subroutine sub_icount(ia,ib,ic,n)
+integer :: n
+integer ia(n), ib(n), ic(n)
+do i=1,n
+ic(i)=ia(i)+ib(i)
+end do
+return
+end
+
+
+program main
+integer,parameter :: n=16384
+integer :: loops
+real(kind=8), allocatable :: a(:), b(:), c(:)
+integer, allocatable :: ia(:), ib(:), ic(:)
+integer is_ok, i, j
+
+loops =  1000
+write(6,'(a,i10)') "<main> n= ", n
+write(6,'(a,i10)') "<main> loops= ", loops
+allocate (a(n), b(n), c(n), stat=is_ok)
+allocate (ia(n), ib(n), ic(n), stat=is_ok)
+
+do i=1,n
+a(i)=real(i)/real(n)
+b(i)=1.0-a(i)
+c(i)=0.0
+end do
+
+do j=1,loops
+call sub_fcount(a,b,c,n)
+call sub_icount(ia,ib,ic,n)
+call sub_ext(a,b,c,n)
+end do
+write(6,'(a,3f8.3)') "<sub_fcount> flop    add:2n, mult:n, div:n each per loop"
+write(6,'(a,3f8.3)') "<sub_icount> integer add:n  each per loop"
+
+deallocate (a)
+deallocate (b)
+deallocate (c)
+
+stop
+end
+
