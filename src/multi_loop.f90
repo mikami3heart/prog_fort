8cba7c2d74903b9431e651ac5fd3edcbabc0351b
mikami3heart <mikami3heart@gmail.com>
2017-01-20 18:54:06 +0900

    check

--- src/multi_loop.f90
+++ src/multi_loop.f90
@@ -5,11 +5,11 @@ real*8 a(n), b(n), c(n)
 a123=a(1)+a(2)+a(3)
 b123=b(1)+b(2)+b(3)
 
-do i=1,n
-c(i)=0.1*(a(i)+b(i))
-do j=i,n
-c(j)= c(j) + (a(j)*a(i)+b(j)*b(i))
-end do
+do i=1,n-1
+	c(i)=0.1*(a(i)+b(i))
+	do j=i,n-1
+		c(j)= c(j) + (a(j)*a(i)+b(j)*b(i))
+	end do
 end do
 
 c(n)=c(n-1)+a123+b123
